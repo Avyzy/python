4a8ea315e3cd610d17711fd3adab84761a069ed8
Avyzy <tonylindahl321@gmail.com>
2025-11-21 22:15:02 -0600

    Update television.py

--- television.py
+++ television.py
@@ -5,19 +5,23 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        '''set Tv with defualt settings'''
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        '''turning tv off and on'''
         self.__status = not self.__status
 
     def mute(self):
+        '''mutes and unmutes when tv is on'''
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        '''channel goes up in number an resets when it hits max'''
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -25,6 +29,7 @@ class Television:
                 self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        '''channel goes down in number and go back to top when it hits min'''
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -32,6 +37,7 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
+        '''turns volume up only when unmuted'''
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -39,6 +45,7 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
+        '''turns volume down only when unmuted'''
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -46,4 +53,5 @@ class Television:
                 self.__volume -= 1
 
     def __str__(self):
+        '''returns a tring with power, channel and volume'''
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
